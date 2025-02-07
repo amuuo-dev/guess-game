f9e718a2c863bc4ac1d4f9210c8665866b4402b3
muuo <anthonymuuo92@gmail.com>
2025-02-07 06:48:54 +0300

    project setup state

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">ayeee this is react</h1>
+    </div>
+  );
+}
+
+export default App;
